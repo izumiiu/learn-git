1bd80f4a1021aed2c25e5ee71f7c334b884150a2
izumiiu <peerawat.ez@gmail.com>
2026-04-27 02:15:05 +0700

    Boolean State

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,19 @@ function App() {
     {id: 4, name: "tata", gender: "gay"},
     {id: 5, name: "pea", gender: "gay"}
   ]);
+  const[show, setShow] = useState(true); // if true แสดงข้อมูล if false ซ่อนข้อมูล
+
 
   // console.table(data);
   
   return (
     <>
       <h1>จำนวนประชากร {data.length} คน</h1>
+      <button onClick={() => setShow(!show)}>{show ? "ซ่อน" : "แสดง"}</button>
       <ul>
-        {data.map((item) => (
+
+        {/* บรรทัด 24 หมายถึง ถ้าtrueไห้ ทำ ถ้าfalse ไม่ทำอะไรเลย */}
+        {show && data.map((item) => (
           // ต้องกำหนด key properties ด้วย โดย key properties ต้องมีค่าไม่ซ้ำกัน
             <li key={item.id}><h3>{item.id}|{item.name} | {item.gender}</h3></li>
         ))}
