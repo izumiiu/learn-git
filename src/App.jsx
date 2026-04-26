2c0d9c2c6eec47149a7216e40d915d916523a319
izumiiu <peerawat.ez@gmail.com>
2026-04-27 00:52:56 +0700

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useState } from "react";
+
+function App() {
+
+  const name="ก้อง";
+  // const age = 30;
+  const [age, setAge] = useState(30);
+
+  // function add(){
+  //     setAge(age + 1);
+  // }
+
+  
+  return (
+    <>
+      <h1>hello testtest {name}</h1>
+      <h3>ที่อยู่ : {age} ปี</h3>
+      <button onClick={() => setAge(age + 1)}>เพิ่มอายุ</button>
+      <button onClick={() => setAge(age -1)}>ลดอายุ</button>
+      <button onClick={() => setAge(30)}>reset</button>
+    </>
+
+  );
+}
+
+export default App;
